efadfa66f5b5e075d0d9f5f4270cd8e82af025ea
southsea <86471807+southsea3026@users.noreply.github.com>
2022-11-08 21:11:22 +0900

    Update report1.c

--- report1.c
+++ report1.c
@@ -7,17 +7,17 @@
 // 1. 문제제시, 2. 코드, 3. 실행결과 캡처한 화면, 4. 그 외의 것 자유롭게 추가 가능
 
 // 1. 자기가 생각하는 구조체를 한 개 만들고 설명하기
-// #include <stdio.h>
+#include <stdio.h>
 
-// struct report1{ // 구조체 이름
-//     char member_variable1[30]; // 멤버 변수 char
-//     int member_variable2; // 멤버 변수 int
-// } new_report1; // 구조체 별칭
+struct report1{ // 구조체 이름
+    char member_variable1[30]; // 멤버 변수 char
+    int member_variable2; // 멤버 변수 int
+} new_report1; // 구조체 별칭
 
-// int main(void){
-//     struct report1 name = {"가나다", 10};
-//     printf("%s, %d", name.member_variable1, name.member_variable2);
-// }
+int main(void){
+    struct report1 name = {"김형원", 20160195};
+    printf("%s, %d", name.member_variable1, name.member_variable2);
+}
 // 구조체는 사용자가 C언어의 기본 타입을 가지고 새롭게 정의할 수 있는 사용자 정의 타입이다.
 // 구조체는 기본 타입만으로는 나타낼 수 없는 복잡한 데이터를 표현할 수 있다.
 // 배열이 같은 타입의 변수 집합이라면, 구조체는 다양한 타입의 변수 집합을 하나의 타입으로 나타낸 것이다.
@@ -77,39 +77,83 @@
 
 
 // 3.구조체 포인터를 사용한 예시 1개
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
 
-typedef struct _book{
-    char name[20];
-    int page;
-    char price[100];
-} book;
+// typedef struct _book{
+//     char name[20];
+//     int page;
+//     char price[100];
+// } book;
 
-int main(){
-    book *p1 = malloc(sizeof(book)); // 구조체 별칭으로 포인터 선언, 메모리 할당
+// int main(){
+//     book *p1 = malloc(sizeof(book)); // 구조체 별칭으로 포인터 선언, 메모리 할당
     
-    // 화살표 연산자로 구조체 멤버에 접근하여 값 할당
-    strcpy(p1->name, "C언어 정복");
-    p1->page = 280;
-    strcpy(p1->price, "30000원");
+//     // 화살표 연산자로 구조체 멤버에 접근하여 값 할당
+//     strcpy(p1->name, "C언어 정복");
+//     p1->page = 280;
+//     strcpy(p1->price, "30000원");
 
-    // 화살표 연산자로 구조체 멤버에 접근하여 값 출력
-    printf("이름: %s\n", p1->name);
-    printf("쪽 수: %d\n", p1->page);
-    printf("가격: %s\n", p1->price);
+//     // 화살표 연산자로 구조체 멤버에 접근하여 값 출력
+//     printf("이름: %s\n", p1->name);
+//     printf("쪽 수: %d\n", (*p1).page);
+//     printf("가격: %s\n", p1->price);
 
-    free(p1);   // 동적 메모리 해제
+//     free(p1);   // 동적 메모리 해제
 
-    return 0;
-}
+//     return 0;
+// }
 
+// 구조체 별칭을 사용하여 포인터 선언, book *p1과 같이 구조체 별칭으로 포인터를 선언 한 뒤 malloc 함수로 메모리를 할당한다. 이때 할당할 메모리 크기도 sizeof(book)처럼 구조체 별칭으로 구할 수 있다.
+// 구조체 포인터에서 멤버에 접근하려면 p1->name과 같이 화살표 연산자를 사용한다. 괄호와 역참조를 사용하면 .(점)으로 멤버에 접근할 수 있다.
 
 // 4. 구조체 배열을 사용한 예시 2개
 // 4-1)
+// #include <stdio.h>
 
+// typedef struct class{
+//     char no[10];
+//     char name[20];
+//     int grade;
+// } student;
+
+// int main(void){
+//     student stu[3];
+
+//     for(int i = 0; i<3; i++){
+//         printf("학번, 이름, 점수 순으로 입력하세요.\n");
+//         scanf("%s %s %d", stu[i].no, stu[i].name, &stu[i].grade);
+//     }
+    
+//     printf("------------------\n");
+//     printf("점수를 출력합니다.\n");
+
+//     for(int i = 0; i<3; i++){
+//         printf("%s %s %d\n", stu[i].no, stu[i].name, stu[i].grade);
+//     }
+//     return 0;
+// }
 
 
 // 4-2)
+// #include <stdio.h>
 
+// typedef struct {
+//     char name[30];
+//     int age;
+// } Student;
+
+// int main(){
+//     Student wizard[3] = {{.name = "해리 포터"}, {.name = "헤르미온느"}, {.name = "론 위즐리"}};
+
+//     wizard[0].age = 17;
+//     wizard[1].age = 16;
+//     wizard[2].age = 17;
+
+//     printf("이름 : %s / 나이 : %d\n", wizard[0].name, wizard[0].age);
+//     printf("이름 : %s / 나이 : %d\n", wizard[1].name, wizard[1].age);
+//     printf("이름 : %s / 나이 : %d\n", wizard[2].name, wizard[2].age);
+
+//     return 0;
+// }
